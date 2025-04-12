37fcd1deed75b82c1385c97a382995515dcf72aa
Muhammad Friza <118143572+frizalaptop@users.noreply.github.com>
2025-04-12 20:49:24 +0700

    Pertemuan 3

---
+++ web_lanjut_3/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/portfolio', [PageController::class, 'portfolio'])->name('portfolio');
+Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+
+Route::resource('member', MemberController::class);
